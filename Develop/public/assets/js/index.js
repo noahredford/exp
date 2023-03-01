a3c1e671cc9e28d2c19d844dffd1d2e6d10406d1
Noah Redford <noah.redford27@gmail.com>
2023-02-28 21:07:33 -0700

    added npm modules and fs require

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -7,6 +7,7 @@ const express = require('express');
 const path = require('path');
 const PORT = 3001;
 const app = express();
+const fs = require('fs');
 
 app.get('/paths', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
