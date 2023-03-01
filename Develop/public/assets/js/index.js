f53ecffe96523bfb1df39c50117954d33beff2d4
Noah Redford <noah.redford27@gmail.com>
2023-02-28 21:18:25 -0700

    created server.js file

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -3,19 +3,6 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
-const express = require('express');
-const path = require('path');
-const PORT = 3001;
-const app = express();
-const fs = require('fs');
-
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
-
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
