08d8fefdd4cbf720ead855698db14801a6d5199b
Noah Redford <noah.redford27@gmail.com>
2023-03-01 17:07:16 -0700

    finished server.js file

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+
 const express = require('express');
 const path = require('path');
 const PORT = 3001;
 const app = express();
-const fs = require('fs');
+const apiRoute = require('./Routes/api');
+const htmlRoute = require('./Routes/html');
+
+app.use(express.urlencoded({extended: true})); // received help from this link: https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
+app.use(express.json());// received help from this link https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+
+app.listen(PORT, () => {
+  console.log(`Now on PORT ${PORT}`)
+});
 
 
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
-
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
